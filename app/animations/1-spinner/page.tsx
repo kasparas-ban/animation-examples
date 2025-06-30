7d5f328dce0890ea9121b834d82e5ad28cd2ad16
kasparas-ban <kasparas.baniulis@gmail.com>
2025-06-30 20:22:07 +0300

    Initial commit

---
+++ app/animations/1-spinner/page.tsx
@@ -0,0 +1,9 @@
+import Spinner from "@/components/spinner/spinner-css";
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Spinner />
+    </div>
+  );
+}
---
+++ components/spinner/spinner-css.tsx
@@ -0,0 +1,9 @@
+import { Sparkle } from "lucide-react";
+
+export default function SpinnerCSS() {
+  return (
+    <div className="rounded-full h-32 w-72 bg-blue-400">
+      <Sparkle />
+    </div>
+  );
+}
