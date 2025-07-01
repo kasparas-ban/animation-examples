2889e460dce812119683e95e0fee30886a470c78
kasparasb <kasparasb@wix.com>
2025-07-01 20:21:05 +0300

    Added spinner becnhmark page

---
+++ app/animations/1-spinner-benchmark/page.tsx
@@ -0,0 +1,9 @@
+import SpinnerCSS from "@/components/spinner/css/spinner-css";
+
+export default function Page() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <SpinnerCSS />
+    </div>
+  );
+}
