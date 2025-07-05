79e28cd1314ae1a9a5325a189089086fff1a51b7
kasparas-ban <kasparas.baniulis@gmail.com>
2025-07-05 16:53:20 +0300

    Fixed CSS animation performance

--- components/spinner/css/spinner-css.tsx
+++ components/spinner/css/spinner-css.tsx
@@ -4,10 +4,12 @@ import "./spinner.css";
 export default function SpinnerCSS() {
   return (
     <div className="loader rounded-full h-[38px] w-[80px] flex items-center p-1">
-      <Sparkle
-        fill="var(--background)"
-        className="sparkle text-transparent size-7.5"
-      />
+      <div className="sparkle">
+        <Sparkle
+          fill="var(--background)"
+          className="text-transparent size-7.5"
+        />
+      </div>
     </div>
   );
 }
