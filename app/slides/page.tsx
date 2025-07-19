8e37c5c21eaa188d93a70c9a76d797d9d7056fde
kasparasb <kasparasb@wix.com>
2025-07-19 09:25:07 +0300

    Working slides

--- app/slides/page.tsx
+++ app/slides/page.tsx
@@ -1,22 +1,76 @@
 "use client";
 
-import { Deck, DefaultTemplate } from "spectacle";
-import Slide1 from "./slide-css-anim";
-import Slide2 from "./slide-waapi-anim";
-import SlideInitial from "./slide-initial";
-import SlideIntro from "./slide-outline";
-import SlideInspo from "./slide-inspo";
-import SlideBrowser from "./slide-browser";
+import { ReactNode, useEffect, useState } from "react";
+import Image from "next/image";
+import SpinnerCSS from "@/components/spinner/css/spinner-css";
+
+const RELATIVE_WIDTH = 1728;
+const RELATIVE_HEIGHT = 1080;
+
+const slides = [Slide1, Slide8];
+
+function Slide1() {
+  return <SlideBackground idx={1} />;
+}
+
+function Slide8() {
+  return (
+    <SlideBackground idx={8}>
+      <div className="translate-x-[615px] translate-y-[635px]">
+        <SpinnerCSS width={240} />
+      </div>
+    </SlideBackground>
+  );
+}
+
+function SlideBackground({
+  idx,
+  children,
+}: {
+  idx: number;
+  children?: ReactNode;
+}) {
+  return (
+    <div className="h-screen w-screen bg-black flex items-center justify-center">
+      <div
+        className="absolute overflow-hidden"
+        style={{
+          width: RELATIVE_WIDTH,
+          height: RELATIVE_HEIGHT,
+        }}
+      >
+        <Image
+          src={`/slides/slide-${idx}.svg`}
+          alt={`Slide ${idx}`}
+          className="w-full h-full object-contain"
+          width={RELATIVE_WIDTH}
+          height={RELATIVE_HEIGHT}
+          priority
+        />
+        <div className="absolute inset-0">{children}</div>
+      </div>
+    </div>
+  );
+}
 
 export default function Page() {
-  return (
-    <Deck template={<DefaultTemplate />}>
-      <SlideInitial />
-      <SlideInspo />
-      <SlideIntro />
-      <Slide1 />
-      <Slide2 />
-      <SlideBrowser />
-    </Deck>
-  );
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "ArrowRight") {
+        setCurrentSlide((prev) => Math.min(prev + 1, slides.length - 1));
+      } else if (event.key === "ArrowLeft") {
+        setCurrentSlide((prev) => Math.max(prev - 1, 0));
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  return <div>{slides[currentSlide]()}</div>;
 }
