9a6e4df8f3dd142ba8342010350c898995607871
kasparasb <kasparasb@wix.com>
2025-08-01 17:12:59 +0300

    Animation tweak

--- components/text-transition/useTextTokens.tsx
+++ components/text-transition/useTextTokens.tsx
@@ -8,6 +8,12 @@ export default function useTextTokens({ text }: { text: string }) {
         <motion.span
           key={idx}
           layoutId={`token-${idx}`}
+          transition={{
+            layout: {
+              duration: 0.6,
+              ease: [0.25, 1, 0.5, 1],
+            },
+          }}
           // className="leading-[1.2]"
         >
           {token}
